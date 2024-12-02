134d6cc0e49c5d3c7e6cc76c25bd157dfbd3e1fd
ftf2004 <frickthefasak@outlook.com>
2024-12-02 09:12:26 +0000

    egretg

---
+++ CD Lab/Dec2_1.c
@@ -0,0 +1,32 @@
+// Checking if amstrong or not
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, sum = 0, temp, remainder, n = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (temp != 0) {
+        temp /= 10;
+        ++n;
+    }
+
+    temp = num;
+
+    while (temp != 0) {
+        remainder = temp % 10;
+        sum += pow(remainder, n);
+        temp /= 10;
+    }
+
+    if (sum == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
