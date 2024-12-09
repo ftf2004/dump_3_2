2fd0f6a2c00eceadb8ad015f565fe7fe40fb17bf
ftf2004 <frickthefasak@outlook.com>
2024-12-09 09:39:55 +0000

    gerg

--- CD_Lab/Dec9_2_dfa.c
+++ CD_Lab/Dec9_2_dfa.c
@@ -1,48 +1,20 @@
 #include <stdio.h>
-
-int dfa() {
-    char ch;
-    int state = 0; 
-    int accepted = 0;  
-    
-    printf("Enter a string:\n");
-    
-    while (1) {
-        ch = getchar();  
-        
-        if (ch == '\n') 
-            break;
-        
-        switch (state) {
-            case 0:  
-                if (ch == 'a') {
-                    state = 1; 
-                } else {
-                    return 0; 
-                }
-                break;
-            case 1: 
-                if (ch == 'a' || ch == 'b') {
-                    state = 1;  
-                }
-                break;
-        }
-    }
-    
-
-    if (state == 1 && ch == 'b') {
-        accepted = 1;
-    }
-    
-    return accepted;
-}
+#include <string.h>
 
 int main() {
-    if (dfa()) {
-        printf("The string is accepted.\n");
+    char input[100];
+    int len;
+
+    printf("Enter a string: ");
+    scanf("%s", input);
+
+    len = strlen(input);
+
+    if (len >= 2 && input[0] == 'a' && input[len - 1] == 'b') {
+        printf("Accepted\n");
     } else {
-        printf("The string is rejected.\n");
+        printf("Rejected\n");
     }
-    
+
     return 0;
 }
